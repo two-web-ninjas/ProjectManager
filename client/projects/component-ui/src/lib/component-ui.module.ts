b287fe1b8c88662c0f13bfd83b8e4bf892fe696d
ProfessionalNinjaDeveloper <professionalninjadeveloper@gmail.com>
2021-03-13 19:35:30 +0100

    registration for was made

--- client/projects/component-ui/src/lib/component-ui.module.ts
+++ client/projects/component-ui/src/lib/component-ui.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './controls/dashboard/dashboard.component';
 import { RegistrationComponent } from './controls/registration/registration.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [ DashboardComponent, RegistrationComponent ],
   imports: [
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ],
   exports: [ DashboardComponent, RegistrationComponent ]
 })
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentUiModule } from '@project-manager/component-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,8 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    ComponentUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
